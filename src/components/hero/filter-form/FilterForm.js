51bc078096bb64a61f9dc6b400a6faa24ef41dad
ilyobilyo <ilito20000@gmail.com>
2023-03-30 14:37:34 +0300

    modify filter and advanced filter to work with contexts and data from database

--- src/components/hero/filter-form/FilterForm.js
+++ src/components/hero/filter-form/FilterForm.js
@@ -1,12 +1,17 @@
 import { SelectField } from "./select-field/SelectField"
 import styles from './FilterForm.module.css';
-import { useState } from "react";
-import { AdvancedFilter } from "./advancedFilter/AdvancedFilter";
 
+import { useContext, useState } from "react";
+
+import { AdvancedFilter } from "./advancedFilter/AdvancedFilter";
+import { TypeContext } from "../../../contexts/TypeContext";
+import { TownContext } from "../../../contexts/TownContext";
 
 export const FilterForm = () => {
-    const [showAdvanced, setShowAdvanced] = useState(false);
+    const {types} = useContext(TypeContext);
+    const {towns, hoods} = useContext(TownContext);
 
+    const [showAdvanced, setShowAdvanced] = useState(false);
 
     const advancedClickHandler = (e) => {
         e.preventDefault();
@@ -15,20 +20,23 @@ export const FilterForm = () => {
     }
 
     return (
-        <form className={styles.filterForm}>
-            <SelectField title='Вид продажба' type='sellType' id={'sell-type'} icon='fas fa-search-dollar' options={['Продажба', 'Под наем']} />
-            <SelectField title='Вид на имота' type='offerType' id={'offer-type'} icon='far fa-building' options={['1-стаен', '2-стаен', '3-стаен', 'Мезонет', 'Офис']} />
-            <SelectField title='Град' type='town' id={'offer-town'} icon='fas fa-city' options={['Варна', 'София']} />
-            <SelectField title='Квартал' type='hood' id={'offer-hood'} icon='fas fa-map-marked-alt' options={['Владиславово', 'Център']} />
+        types.length === 0 || towns.length === 0 || hoods.length === 0
+            ? <p>Loading...</p>
+            : <form className={styles.filterForm}>
+                <SelectField title='Вид продажба' type={'sellType'} id={'sell-type'} icon='fas fa-search-dollar' options={types.sellType} />
+                <SelectField title='Вид на имота' type={'offerType'} id={'offer-type'} icon='far fa-building' options={types.offerType} />
+                <SelectField title='Град' type={'town'} id={'offer-town'} icon='fas fa-city' options={towns} />
+                <SelectField title='Квартал' type={'hood'} id={'offer-hood'} icon='fas fa-map-marked-alt' options={hoods} />
 
-            {showAdvanced 
-            ? <AdvancedFilter hideClickHandler={advancedClickHandler}/>
-            : <a href="#/" onClick={advancedClickHandler}>
-                <i className="fas fa-caret-down" /> Advanced
-              </a>
-            }
-            
-            <button className={styles.btnSearch}>Търси</button>
-        </form>
+                {showAdvanced
+                    ? <AdvancedFilter hideClickHandler={advancedClickHandler} types={types}/>
+                    : <a href="#/" onClick={advancedClickHandler}>
+                        <i className="fas fa-caret-down" /> Advanced
+                    </a>
+                }
+
+                <button className={styles.btnSearch}>Търси</button>
+            </form>
+
     )
 }
--- src/components/hero/filter-form/advancedFilter/AdvancedFilter.js
+++ src/components/hero/filter-form/advancedFilter/AdvancedFilter.js
@@ -1,17 +1,23 @@
+import { useContext } from 'react'
+
+import { TypeContext } from '../../../../contexts/TypeContext'
+
 import { InputField } from '../input-field/InputField'
 import { SelectField } from '../select-field/SelectField'
 import styles from './AdvancedFilter.module.css'
 
 export const AdvancedFilter = ({hideClickHandler}) => {
+    const {types} = useContext(TypeContext);
+
     return (
         <div className={styles.advanced}>
             <InputField title='Цена от' id={'price-from'} icon={'fas fa-money-bill-wave'} />
             <InputField title='Цена до' id={'price-to'} icon={'fas fa-money-bill-wave'} />
 
-            <SelectField title='Обзавеждане' type='furniture' id={'offer-furniture'} icon='fas fa-couch' options={['Необзаведен', 'Ново обзавеждане', 'Старо обзавеждане']} />
-            <SelectField title='Отопление' type='heating' id={'offer-heating'} icon='fas fa-temperature-high' options={['ТЕЦ', 'Електричество']} />
-            <SelectField title='Изглед' type='exposition' id={'offer-exposition'} icon='fas fa-compass' options={['Изток', 'Запад', 'Север', 'Юг']} />
-            <SelectField title='Конструкция' type='construction' id={'offer-construction'} icon='fas fa-tools' options={['ЕПК', 'ПК', 'Тухла']} />
+            <SelectField title='Обзавеждане' type='furniture' id={'offer-furniture'} icon='fas fa-couch' options={types.furniture} />
+            <SelectField title='Отопление' type='heating' id={'offer-heating'} icon='fas fa-temperature-high' options={types.Heating} />
+            <SelectField title='Изглед' type='exposition' id={'offer-exposition'} icon='fas fa-compass' options={types.exposition} />
+            <SelectField title='Конструкция' type='construction' id={'offer-construction'} icon='fas fa-tools' options={types.construction} />
 
             <InputField title='Квадратура ㎡' id={'qaud'} />
             <a href="#/" onClick={hideClickHandler}>
