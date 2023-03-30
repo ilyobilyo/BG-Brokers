e46b68b9204bd5d94ce068bf2840c18b25963857
ilyobilyo <ilito20000@gmail.com>
2023-03-30 14:36:21 +0300

    Create TypeContext and TownContext

---
+++ src/contexts/TypeContext.js
@@ -0,0 +1,21 @@
+import { createContext, useEffect, useState } from "react";
+import { GetAllTypes } from "../services/typeService";
+
+export const TypeContext = createContext();
+
+export const TypeProvider = ({children}) => {
+    const [types, setTypes] = useState([]);
+
+    useEffect(() => {
+        GetAllTypes()
+            .then(data => {
+                setTypes(data);
+            });
+    }, [])
+
+    return (
+        <TypeContext.Provider value={{types}}>
+            {children}
+        </TypeContext.Provider>
+    )
+}
