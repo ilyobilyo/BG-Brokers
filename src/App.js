1253081c991b877ad8f86ace63b27ad39db55a65
ilyobilyo <ilito20000@gmail.com>
2023-03-30 14:37:53 +0300

    apply Type and Town Providers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import { Header } from './components/header/Header';
 import { Hero } from './components/hero/Hero';
+import { TownProvider } from './contexts/TownContext';
+import { TypeProvider } from './contexts/TypeContext';
 
 function App() {
   return (
     <div className="App">
       <Header />
 
-      <Hero />
+      <TypeProvider>
+        <TownProvider>
+          
+          <Hero />
+
+        </TownProvider>
+      </TypeProvider>
     </div>
   );
 }
