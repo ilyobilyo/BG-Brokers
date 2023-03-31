b170a73ac5ac0915fccbed10223144653aa58033
ilyobilyo <ilito20000@gmail.com>
2023-03-31 17:28:47 +0300

    Add different events in Select and Input Field to make my filter form under control

--- src/components/hero/filter-form/input-field/InputField.js
+++ src/components/hero/filter-form/input-field/InputField.js
@@ -1,12 +1,12 @@
 import styles from './InputField.module.css';
 
-export const InputField = ({title, icon, id}) => {
+export const InputField = ({title, icon, id, name, onChangeHandler, validatePrice}) => {
     return (
         <div className={styles.inputContainer}>
             <label htmlFor={id}>
                 <i className={icon} /> {title}
             </label>
-            <input type="text" id={id}/>
+            <input type="text" name={name} id={id} onChange={onChangeHandler} onBlur={validatePrice}/>
         </div>
     )
 }
--- src/components/hero/filter-form/select-field/SelectField.js
+++ src/components/hero/filter-form/select-field/SelectField.js
@@ -1,14 +1,14 @@
 import { FilterSelectOption } from "../filter-select-option/FilterSelectOption"
 import styles from './SelectField.module.css';
 
-export const SelectField = ({title, id, type, icon, options}) => {
+export const SelectField = ({title, id, type, icon, options, optionValues, onChangeHandler, onBlurHndler}) => {
     return (
         <div className={styles.inputContainer}>
             <label htmlFor={id}>
                 <i className={icon} /> {title}
             </label>
-            <select name={type} id={id}>
-                {options.map(x => <FilterSelectOption key={x.name} value={x.name} text={x.name}/>)}
+            <select name={type} id={id} onChange={onChangeHandler} onBlur={onBlurHndler}>
+                {options.map(x => <FilterSelectOption key={x.name} value={optionValues ? x.id : x.name} text={x.name}/>)}
             </select>
         </div>
     )
