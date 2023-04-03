3c48b67b0d1647152e8938e2bbee294adfcdf2ba
ilyobilyo <ilito20000@gmail.com>
2023-04-03 18:25:05 +0300

    Add firebase auth in project

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage'
+import { getAuth } from "firebase/auth";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBDzU20bP8iluQo_dEXYMIrKitMcX-Hgyw",
@@ -18,3 +20,5 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
 export const storage = getStorage(app);
+
+export const auth = getAuth(app);
