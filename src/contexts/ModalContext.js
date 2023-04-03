44dc1d1659472079799f84a0667b3fe5c0afaff6
ilyobilyo <ilito20000@gmail.com>
2023-04-03 15:06:50 +0300

    Add context for all modals in the application

---
+++ src/contexts/ModalContext.js
@@ -0,0 +1,30 @@
+import { createContext, useEffect, useRef, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
+export const ModalContext = createContext();
+
+export const ModalContextProvider = ({ children }) => {
+    const [isOpen, setIsOpen] = useState(true);
+    const modalRef = useRef();
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (event.target.tagName === 'SECTION') {
+                navigate('/');
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [modalRef]);
+
+    return (
+        <ModalContext.Provider value={{ isOpen, modalRef }}>
+            {children}
+        </ModalContext.Provider>
+    )
+}
