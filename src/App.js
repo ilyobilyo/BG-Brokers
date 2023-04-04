a410ae461a6e1052dae8daa2234707b8b78d4a42
ilyobilyo <ilito20000@gmail.com>
2023-04-04 17:09:41 +0300

    Add logout path

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Home } from './components/home/Home';
 import { ModalContextProvider } from './contexts/ModalContext';
 import { Login } from './components/login/Login';
 import { AuthProvider } from './contexts/AuthContext';
+import { Logout } from './components/logout/Logout';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
                 <Route path="/" element={<Outlet />}/>
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/logout" element={<Logout />} />
               </Routes>
             </OfferLocationProvider>
           </TypeProvider>
