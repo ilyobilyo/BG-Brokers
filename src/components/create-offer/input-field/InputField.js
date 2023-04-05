cb27369935d727e581993e3ca36a62edeb8faf35
ilyobilyo <ilito20000@gmail.com>
2023-04-05 18:48:09 +0300

    Add input field component for create offer form

---
+++ src/components/create-offer/input-field/InputField.js
@@ -0,0 +1,15 @@
+import styles from '../CreateOffer.module.css';
+
+export const InputField = ({ elementId, labelText, inputType, name, onChanegeHandler, isMultiple }) => {
+    return (
+        isMultiple
+            ? <div className={`${styles.inputWrapper} ${styles.imagesInput}`}>
+                <label htmlFor={elementId}>{labelText}</label>
+                <input type={inputType} name={name} id={elementId} onChange={onChanegeHandler} multiple />
+            </div>
+            : <div className={styles.inputWrapper}>
+                <label htmlFor={elementId}>{labelText}</label>
+                <input type={inputType} name={name} id={elementId} onChange={onChanegeHandler} />
+            </div>
+    )
+}
