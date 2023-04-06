c5f4912af44aa6a008edc5c72686e4791e1d5d05
ilyobilyo <ilito20000@gmail.com>
2023-04-06 17:04:48 +0300

    Update manageUsers component to work with users from database

--- src/components/manage-users/ManageUser.js
+++ src/components/manage-users/ManageUser.js
@@ -1,19 +1,29 @@
 import { UserCard } from "./user-card/UserCard"
 import styles from './ManageUsers.module.css'
+import { useEffect, useState } from "react"
+import * as userService from '../../services/userService';
 
 export const ManageUsers = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        userService.getAllUsers()
+            .then(data => {
+                setUsers(data);
+            })
+    }, [])
+
     return (
         <section>
             <div className={styles.manageUsersContent}>
                 <h1>Manage users</h1>
-                <div className={styles.cardWrapper}>
-                    <UserCard />
-                    <UserCard />
-                    <UserCard />
-                    <UserCard />
-                    <UserCard />
-                    <UserCard />
-                </div>
+                {users.length === 0
+                    ? <p>Loading...</p>
+                    : <div className={styles.cardWrapper}>
+                        {users.map(x => <UserCard key={x.id} user={x}/>)}
+                    </div>
+                }
+
             </div>
         </section>
 
