0fbc592ca64edcfb787504b56c26e6d87c8d7c02
ilyobilyo <ilito20000@gmail.com>
2023-04-06 17:06:11 +0300

    Update isAuthenticated state to change on login and on logout

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,21 +1,30 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { useLocalStorage } from '../hooks/useLocalStorage'
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useLocalStorage('auth', {});
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    useEffect(() => {
+        if (user.accessToken) {
+            setIsAuthenticated(true);
+        }
+    })
 
     const onLogin = (user) => {
-        setUser(user)
+        setUser(user);
+        setIsAuthenticated(true);
     }
 
     const onLogout = () => {
         setUser({})
+        setIsAuthenticated(false);
     }
 
     return (
-        <AuthContext.Provider value={{ user, onLogin, onLogout, isAuthenticated: !!user.accessToken}}>
+        <AuthContext.Provider value={{ user, onLogin, onLogout, isAuthenticated}}>
             {children}
         </AuthContext.Provider>
     )
