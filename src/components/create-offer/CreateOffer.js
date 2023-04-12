ab2bf57d708cd049a045fed3dc40660e9b97456a
ilyobilyo <ilito20000@gmail.com>
2023-04-12 16:42:47 +0300

    Add addNewOffer function from OfferContext, add useNavigate hook, add isLoading state, add onSubmit handler in CreateOffer component

--- src/components/create-offer/CreateOffer.js
+++ src/components/create-offer/CreateOffer.js
@@ -6,12 +6,18 @@ import { InputField } from './input-field/InputField';
 import { SelectField } from './select-field/SelectField';
 import { Textarea } from './textarea/Textarea';
 import * as userService from '../../services/userService';
+import * as offerService from '../../services/offerService';
+import { useNavigate } from 'react-router-dom';
+import { OfferContext } from '../../contexts/OfferContext';
 
 
 export const CreateOffer = () => {
-    const {types} = useContext(TypeContext);
-    const {towns, hoods, getTownHoods} = useContext(OfferLocationContext);
+    const { types } = useContext(TypeContext);
+    const { towns, hoods, getTownHoods } = useContext(OfferLocationContext);
+    const {addNewOffer} = useContext(OfferContext);
+    const navigate = useNavigate();
 
+    const [isLoading, setIsLoading] = useState(false);
     const [brokers, setBrokers] = useState([]);
     const [formData, setFormData] = useState({
         title: '',
@@ -41,12 +47,12 @@ export const CreateOffer = () => {
 
     useEffect(() => {
         userService.getAllUsers()
-        .then(data => {
-            setBrokers(data);
-        })
+            .then(data => {
+                setBrokers(data);
+            })
     }, [])
 
-    
+
 
     const onChange = (e) => {
         setFormData(state => ({
@@ -81,57 +87,74 @@ export const CreateOffer = () => {
         getTownHoods(formData.townId)
     }
 
+    const onSubmit = (e) => {
+        e.preventDefault();
+        setIsLoading(true);
+
+        const broker = brokers.find(x => x.id === formData.broker);
+        formData.broker = broker;
+
+        offerService.createOffer(formData)
+            .then(newOffer => {
+                setIsLoading(false);
+                addNewOffer(newOffer);
+                navigate('/');
+            })
+    }
+
     return (
         <section id="createModal" className={styles.modal}>
             <div className={styles.createOfferContent}>
                 <h1>Create Offer</h1>
-                <form encType="multipart/form-data" className={styles.createOfferForm}>
-                    <InputField elementId='offer-title' labelText='Title' inputType='text' name='title' onChanegeHandler={onChange}/>
-                    
-                    <SelectField elementId='city' labelText='Town' name='town' options={towns} hasOptionId={true} onChangeHandler={onChangeTownInput} onBlurHandler={setTownHoods}/>
-                    
-                    <SelectField elementId='offer-hood' labelText='Hood' name='hood' options={hoods} onChangeHandler={onChange}/>
+                {isLoading ? <p>Creating ...</p>
+                    : <form encType="multipart/form-data" className={styles.createOfferForm} onSubmit={onSubmit}>
+                        <InputField elementId='offer-title' labelText='Title' inputType='text' name='title' onChanegeHandler={onChange} />
 
-                    <SelectField elementId='sell-type' labelText='Sell Type' name='sellType' options={types.sellType} onChangeHandler={onChange}/>
-                    
-                    <SelectField elementId='offer-type' labelText='Offer Type' name='offerType' options={types.offerType} onChangeHandler={onChange}/>
-                    
-                    <SelectField elementId='offer-construction' labelText='Construction' name='construction' options={types.construction} onChangeHandler={onChange}/>
-                    
-                    <SelectField elementId='offer-furniture' labelText='Furniture' name='furniture' options={types.furniture} onChangeHandler={onChange}/>
+                        <SelectField elementId='city' labelText='Town' name='town' options={towns} hasOptionId={true} onChangeHandler={onChangeTownInput} onBlurHandler={setTownHoods} />
 
-                    <SelectField elementId='offer-heating' labelText='Heating' name='heating' options={types.heating} onChangeHandler={onChange}/>
-                    
-                    <SelectField elementId='offer-broker' labelText='Broker' name='broker' options={brokers} hasOptionId={true} onChangeHandler={onChange}/>
+                        <SelectField elementId='offer-hood' labelText='Hood' name='hood' options={hoods} onChangeHandler={onChange} />
 
-                    <InputField elementId='offer-address' labelText='Address' inputType='text' name='address' onChanegeHandler={onChange}/>
-                   
-                    <InputField elementId='offer-price' labelText='Price' inputType='number' name='price' onChanegeHandler={onChange}/>
-                    
-                    <InputField elementId='offer-area' labelText='Area in ㎡' inputType='number' name='area' onChanegeHandler={onChange}/>
-                    
-                    <InputField elementId='offer-rooms' labelText='Total Rooms' inputType='number' name='rooms' onChanegeHandler={onChange}/>
-                    
-                    <InputField elementId='offer-bedrooms' labelText='Bedrooms' inputType='number' name='bedrooms' onChanegeHandler={onChange}/>
+                        <SelectField elementId='sell-type' labelText='Sell Type' name='sellType' options={types.sellType} onChangeHandler={onChange} />
 
-                    <InputField elementId='offer-terraces' labelText='Terraces' inputType='number' name='terraces' onChanegeHandler={onChange}/>
-                    
-                    <InputField elementId='offer-floor' labelText='Floor' inputType='number' name='floor' onChanegeHandler={onChange}/>
+                        <SelectField elementId='offer-type' labelText='Offer Type' name='offerType' options={types.offerType} onChangeHandler={onChange} />
 
-                    <InputField elementId='offer-total-floors' labelText='Total Floors' inputType='number' name='totalFloors' onChanegeHandler={onChange}/>
-                    
-                    <InputField elementId='offer-owner-phone' labelText='Owner phone' inputType='text' name='ownerPhone' onChanegeHandler={onChange}/>
-                    
-                    <InputField elementId='offer-elevator' labelText='Has Elevator' inputType='checkbox' name='hasElevator' onChanegeHandler={onChangeCheckbox}/>
-                    
-                    <InputField elementId='offer-parking' labelText='Has Parking place' inputType='checkbox' name='hasParkingPlace' onChanegeHandler={onChangeCheckbox}/>
-                   
-                    <Textarea elementId='offer-desc' labelText='Description' name='description' onChangeHandler={onChange}/>
-                    
-                    <InputField elementId='offer-images' labelText='Images' inputType='file' name='images' onChanegeHandler={onChangeFileInput} isMultiple={true}/>
-                    
-                    <button className={styles.btnSubmit}>Create</button>
-                </form>
+                        <SelectField elementId='offer-construction' labelText='Construction' name='construction' options={types.construction} onChangeHandler={onChange} />
+
+                        <SelectField elementId='offer-furniture' labelText='Furniture' name='furniture' options={types.furniture} onChangeHandler={onChange} />
+
+                        <SelectField elementId='offer-heating' labelText='Heating' name='heating' options={types.heating} onChangeHandler={onChange} />
+
+                        <SelectField elementId='offer-broker' labelText='Broker' name='broker' options={brokers} hasOptionId={true} onChangeHandler={onChange} />
+
+                        <InputField elementId='offer-address' labelText='Address' inputType='text' name='address' onChanegeHandler={onChange} />
+
+                        <InputField elementId='offer-price' labelText='Price' inputType='number' name='price' onChanegeHandler={onChange} />
+
+                        <InputField elementId='offer-area' labelText='Area in ㎡' inputType='number' name='area' onChanegeHandler={onChange} />
+
+                        <InputField elementId='offer-rooms' labelText='Total Rooms' inputType='number' name='rooms' onChanegeHandler={onChange} />
+
+                        <InputField elementId='offer-bedrooms' labelText='Bedrooms' inputType='number' name='bedrooms' onChanegeHandler={onChange} />
+
+                        <InputField elementId='offer-terraces' labelText='Terraces' inputType='number' name='terraces' onChanegeHandler={onChange} />
+
+                        <InputField elementId='offer-floor' labelText='Floor' inputType='number' name='floor' onChanegeHandler={onChange} />
+
+                        <InputField elementId='offer-total-floors' labelText='Total Floors' inputType='number' name='totalFloors' onChanegeHandler={onChange} />
+
+                        <InputField elementId='offer-owner-phone' labelText='Owner phone' inputType='text' name='ownerPhone' onChanegeHandler={onChange} />
+
+                        <InputField elementId='offer-elevator' labelText='Has Elevator' inputType='checkbox' name='hasElevator' onChanegeHandler={onChangeCheckbox} />
+
+                        <InputField elementId='offer-parking' labelText='Has Parking place' inputType='checkbox' name='hasParkingPlace' onChanegeHandler={onChangeCheckbox} />
+
+                        <Textarea elementId='offer-desc' labelText='Description' name='description' onChangeHandler={onChange} />
+
+                        <InputField elementId='offer-images' labelText='Images' inputType='file' name='images' onChanegeHandler={onChangeFileInput} isMultiple={true} />
+
+                        <button className={styles.btnSubmit}>Create</button>
+                    </form>}
+
             </div>
         </section>
 
