08f6d3d7c8750e35d31b96ea91d7dde918d6ee6e
ilyobilyo <ilito20000@gmail.com>
2023-04-18 19:01:14 +0300

    Add updateOffer function in OfferContext to update offers in offers state. Replace old offer with updated offer in state

--- src/contexts/OfferContext.js
+++ src/contexts/OfferContext.js
@@ -17,8 +17,18 @@ export const OfferProvider = ({ children }) => {
         setOffers(state => [offer, ...state]);
     }
 
+    const updateOffer = (offer) => {
+        const currOffers = [...offers];
+        const offerIndex = offers.findIndex(x => x.id === offer.id);
+
+        if (offerIndex !== -1) {
+            currOffers[offerIndex] = offer;
+            setOffers(currOffers);
+        }
+    }
+
     return (
-        <OfferContext.Provider value={{offers, addNewOffer}}>
+        <OfferContext.Provider value={{offers, addNewOffer, updateOffer}}>
             {children}
         </OfferContext.Provider>
     )
