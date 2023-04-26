b63122dbfd3a1340cb6cc128e5420a9fbcfc1115
ilyobilyo <ilito20000@gmail.com>
2023-04-26 19:37:18 +0300

    Add CreateType route, add AuthenticatedGuard for login and register, move edit user route in PrivateGuard

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ import { EditOffer } from './components/edit-offer/EditOffer';
 import { AdminGuard } from './components/common/AdminGuard';
 import { EditUser } from './components/manage-users/edit-user/EditUser';
 import { MyProfile } from './components/my-profile/MyProfile';
+import { CreateType } from './components/create-type/CreateType';
+import { AuthenticatedGuard } from './components/common/AuthenticatedGuard';
 
 function App() {
   const location = useLocation();
@@ -32,32 +34,44 @@ function App() {
             <OfferLocationProvider>
               <OfferProvider>
                 <Routes>
+
                   <Route path="/" element={<Home />}>
                     <Route path="/create" element={<CreateOffer />} />
+                    <Route path='/createType' element={<CreateType />} />
                     <Route path='/offer/:offerId' element={<OfferModal />} />
                     <Route path='/edit/:offerId' element={<EditOffer />} />
                   </Route>
 
-                  <Route path='/account/register' element={<Register />} />
-                  <Route path="/account/login" element={<Login />} />
-                  <Route element={<AdminGuard />}>
-                    <Route path="/manageUsers" element={<ManageUsers />} />
-                    <Route path="/editUser/:userId" element={<EditUser />} />
+                  <Route element={<AuthenticatedGuard />}>
+                    <Route path='/account/register' element={<Register />} />
+                    <Route path="/account/login" element={<Login />} />
                   </Route>
+
                   <Route element={<PrivateGuard />}>
                     <Route path="/logout" element={<Logout />} />
                     <Route path='/myProfile' element={<MyProfile />}>
                       <Route path='offer/:offerId' element={<OfferModal />} />
+                      <Route path='edit/:offerId' element={<EditOffer />} />
+
                     </Route>
+                    <Route path="/editUser/:userId" element={<EditUser />} />
                   </Route>
+
+                  <Route element={<AdminGuard />}>
+                    <Route path="/manageUsers" element={<ManageUsers />} />
+                    <Route path="/editUser/:userId" element={<EditUser />} />
+                  </Route>
+
                 </Routes>
                 {background && (
                   <Routes>
                     <Route path="/create" element={<CreateOffer />} />
                     <Route path='/offer/:offerId' element={<OfferModal />} />
                     <Route path='myProfile/offer/:offerId' element={<OfferModal />} />
+                    <Route path='myProfile/edit/:offerId' element={<EditOffer />} />
                     <Route path='/edit/:offerId' element={<EditOffer />} />
                     <Route path="/editUser/:userId" element={<EditUser />} />
+                    <Route path='/createType' element={<CreateType />} />
                   </Routes>
                 )}
               </OfferProvider>
