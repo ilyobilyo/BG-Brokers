de6398deb8f99b25b3dfec8344fcb26c271f9b3b
ilyobilyo <ilito20000@gmail.com>
2023-05-11 17:48:18 +0300

    Order all hoods when make query to db

--- src/services/hoodService.js
+++ src/services/hoodService.js
@@ -1,10 +1,10 @@
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
 import { db } from "../firebase";
 
 const hoodRef = collection(db, 'Hoods');
 
 export async function GetHoodsForTown(townId) {
-    const q = query(hoodRef, where("townId", "==", townId));
+    const q = query(hoodRef, where("townId", "==", townId), orderBy("name"));
 
     const querySnapshot = await getDocs(q);
 
