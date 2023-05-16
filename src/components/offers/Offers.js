19710890333836676716f32cde906cc46ad753aa
ilyobilyo <ilito20000@gmail.com>
2023-05-16 16:40:07 +0300

    Add lastDoc state from OfferContext. Add useEffect hook that depend on lastDoc state for updating next offers from database and implement infinite scroll

--- src/components/offers/Offers.js
+++ src/components/offers/Offers.js
@@ -1,16 +1,36 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import styles from './Offers.module.css';
 import { OfferCard } from './offer-card/OfferCard';
 import { OfferContext } from '../../contexts/OfferContext';
 
 export const Offers = () => {
-    const {offers} = useContext(OfferContext);
+    const { lastDoc, offers, getOffersForInfiniteScroll } = useContext(OfferContext);
+
+    useEffect(() => {
+        if (lastDoc) {
+            window.addEventListener("scroll", handleScroll);
+            console.log('pls');
+        }
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [lastDoc]);
+
+    const handleScroll = () => {
+        if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+            console.log("dOCC", lastDoc);
+            getOffersForInfiniteScroll(lastDoc, handleScroll)
+        }
+    };
 
     return (
         <section className={styles.topOffers}>
             <h2 className={styles.topOffersHeading}>Top Offers</h2>
             <div className={styles.topOffersContainer}>
-                {offers.map(x => <OfferCard key={x.id} offer={x}/>)}
+                {offers &&
+                    offers.map(x => <OfferCard key={x.id} offer={x} />)
+                }
             </div>
         </section>
     )
