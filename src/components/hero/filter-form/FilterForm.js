4a525674501a555dff23d3b72d4263b10d5962ba
ilyobilyo <ilito20000@gmail.com>
2023-05-17 18:07:48 +0300

    Add filter logic in filter component

--- src/components/hero/filter-form/FilterForm.js
+++ src/components/hero/filter-form/FilterForm.js
@@ -1,26 +1,30 @@
 import { SelectField } from "./select-field/SelectField"
 import styles from './FilterForm.module.css';
 
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import { AdvancedFilter } from "./advancedFilter/AdvancedFilter";
 import { TypeContext } from "../../../contexts/TypeContext";
 import { OfferLocationContext } from "../../../contexts/OfferLocationContext";
 import { checkPrice } from "../../../utils/validate";
+import { OfferContext } from "../../../contexts/OfferContext";
+import { useNavigate } from "react-router-dom";
 
 export const FilterForm = () => {
-    const {types} = useContext(TypeContext);
-    const {towns, hoods, getTownHoods} = useContext(OfferLocationContext);
-
+    const { types } = useContext(TypeContext);
+    const { towns, hoods, getTownHoods } = useContext(OfferLocationContext);
+    const { getInitialOffers} = useContext(OfferContext);
+    const navigate = useNavigate();
+    
     const [errors, setErrors] = useState({
         priceFrom: '',
-        priceTo:'',
+        priceTo: '',
         quadrature: '',
     })
     const [formData, setFormData] = useState({
         sellType: '',
         offerType: '',
-        town: '',
+        townId: '',
         hood: '',
         priceFrom: '',
         priceTo: '',
@@ -53,52 +57,71 @@ export const FilterForm = () => {
     }
 
     const setTownHoods = (e) => {
-        getTownHoods(formData.town)
+        getTownHoods(formData.townId)
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const queryParams = new URLSearchParams();
+        let params = {};
+        
+        for (const [key, value] of Object.entries(formData)) {
+            if (value !== "") {
+                queryParams.append(key, value);
+                params[key] = value;
+            }
+        }
+
+        const filteredURL = `?${queryParams.toString()}`;
+        
+        getInitialOffers(params)
+
+        navigate(filteredURL);
     }
 
     return (
         types === undefined || towns.length === 0
             ? <p>Loading...</p>
-            : <form className={styles.filterForm}>
-                <SelectField 
-                    title='Вид продажба' 
-                    type={'sellType'} 
-                    id={'sell-type'} 
-                    icon='fas fa-search-dollar' 
+            : <form className={styles.filterForm} onSubmit={onSubmit} method="get">
+                <SelectField
+                    title='Вид продажба'
+                    type={'sellType'}
+                    id={'sell-type'}
+                    icon='fas fa-search-dollar'
                     options={types.sellType}
                     onChangeHandler={onChange}
                 />
-                <SelectField 
-                    title='Вид на имота' 
-                    type={'offerType'} 
-                    id={'offer-type'} 
-                    icon='far fa-building' 
+                <SelectField
+                    title='Вид на имота'
+                    type={'offerType'}
+                    id={'offer-type'}
+                    icon='far fa-building'
                     options={types.offerType}
                     onChangeHandler={onChange}
                 />
-                <SelectField 
-                    title='Град' 
-                    type={'town'} 
-                    id={'offer-town'} 
-                    icon='fas fa-city' 
-                    options={towns} 
+                <SelectField
+                    title='Град'
+                    type={'townId'}
+                    id={'offer-town'}
+                    icon='fas fa-city'
+                    options={towns}
                     optionValues={Object.keys(towns)}
                     onChangeHandler={onChange}
                     onBlurHndler={setTownHoods}
                 />
-                <SelectField 
-                    title='Квартал' 
-                    type={'hood'} 
-                    id={'offer-hood'} 
-                    icon='fas fa-map-marked-alt' 
-                    options={hoods} 
+                <SelectField
+                    title='Квартал'
+                    type={'hood'}
+                    id={'offer-hood'}
+                    icon='fas fa-map-marked-alt'
+                    options={hoods}
                     onChangeHandler={onChange}
                 />
 
                 {showAdvanced
-                    ? <AdvancedFilter 
-                        hideClickHandler={advancedClickHandler} 
-                        types={types} 
+                    ? <AdvancedFilter
+                        hideClickHandler={advancedClickHandler}
+                        types={types}
                         onChangeHandler={onChange}
                         validatePrice={validatePrice}
                     />
